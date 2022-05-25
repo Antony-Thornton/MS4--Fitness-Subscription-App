2b454b8ec15dd9f25878b8b25e83a6dd9cbac317
Antony-Thornton <Antony_thornton_p@outlook.com>
2022-05-25 20:28:40 +0000

    Added a checkout form and connected it.

--- cart/templatetags/bag_tools.py
+++ cart/templatetags/bag_tools.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name='sub_total')
-def sub_total(price, quantity):
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
     return price * quantity
--- fitness_subscription_app/settings.py
+++ fitness_subscription_app/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'home',
     'products',
     'cart',
+    'checkout',
+
+    # Other
+    'crispy_forms',
 
 ]
 
@@ -61,6 +65,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'fitness_subscription_app.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -75,8 +81,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.context.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
