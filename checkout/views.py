e659b31b100d29a7404e452c2e9e54366f7cd781
Antony-Thornton <Antony_thornton_p@outlook.com>
2022-05-26 20:14:25 +0000

    Added stripe functionality and formatting.

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51L3mmiAZkreFJa2DjHbK2ObZLeBJDaBzIBPQnm08lUT0xt38NqTlPzPSYU62OW35K488UVORFT7PurvYMpaxFIn8007hSXNcQJ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
