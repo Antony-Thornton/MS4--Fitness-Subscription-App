5ad8afba0319daadb8cbb3e4edb738ca817c36f0
Antony-Thornton <Antony_thornton_p@outlook.com>
2022-06-02 15:51:13 +0000

    Added functionality to submit contact form to admin.

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from .forms import ContactForm
 from .models import contact_message
 
 from django.contrib import messages
@@ -40,6 +41,24 @@ def contact_form_post(request):
         messages.success(request, f'Thank you {message_name} for your message. We \
                          will get back to you as soon \
                          as possible via {message_email}.')
+
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'message': request.POST['message'],
+        }
+
+        contact_form = ContactForm(form_data)
+        if contact_form.is_valid():
+            contact = contact_form.save(commit=False)
+            contact.save()
+            request.session['save_info'] = 'save-info' in request.POST
+            return render(request, template, {})
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please resubmit')
+        
+        
         return render(request, template, {})
         #  return render(request, template, {'message_name': message_name, }) 
         #  If you were to stay on the contact page.
