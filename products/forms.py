6bcc59fd45bb2353cf3344c97700517317ca1e3c
Antony-Thornton <Antony_thornton_p@outlook.com>
2022-06-04 14:39:32 +0000

    Added image preview to edit form.

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import product_review
 from .models import Product, Category
+from .widgets import CustomClearableFileInput
 
 
 class ReviewForm(forms.ModelForm):
@@ -35,6 +36,8 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
