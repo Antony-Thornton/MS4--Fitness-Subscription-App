780ff28426af8398694a9dd53e9aeb26d9e4ab72
Antony-Thornton <Antony_thornton_p@outlook.com>
2022-06-04 14:11:42 +0000

    Added links for editing and deleting products.

--- products/views.py
+++ products/views.py
@@ -87,14 +87,15 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Successfully added a new product.')
-            return redirect(reverse('add_product'))
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. \
-                Please check the form is valid.')
-
-    form = ProductForm()
+                Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
@@ -126,6 +127,13 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
 
 # Moved this to the function above.
 # def productReview(request):
