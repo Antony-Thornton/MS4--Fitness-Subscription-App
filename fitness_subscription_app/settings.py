3a9ef700fff492dc8364a521da4d1196aad67b8d
Antony-Thornton <Antony_thornton_p@outlook.com>
2022-06-07 20:46:49 +0000

    Fixed line issues.
    Use # noqa: E501 if you need to keep the line on one line.

--- fitness_subscription_app/settings.py
+++ fitness_subscription_app/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = \
-    'django-insecure-xeo#qrgx-tb+$)r9(awg#tecm$9w#ssf-)jw2t_s^d(=qb#(ly'
+SECRET_KEY = 'django-insecure-xeo#qrgx-tb+$)r9(awg#tecm$9w#ssf-)jw2t_s^d(=qb#(ly'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False  # False on deployment
