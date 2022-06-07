a8bdbc7da5374aa220c48c43bd0665516fd7e4bc
Antony-Thornton <Antony_thornton_p@outlook.com>
2022-06-07 21:02:06 +0000

    Re ran step 9 of readme.

--- fitness_subscription_app/settings.py
+++ fitness_subscription_app/settings.py
@@ -129,15 +129,15 @@ WSGI_APPLICATION = 'fitness_subscription_app.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+     }
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+         'default': {
+             'ENGINE': 'django.db.backends.sqlite3',
+             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         }
+     }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
