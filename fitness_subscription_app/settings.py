2d0908bb5a589bf92a76961254d4741c767689aa
Antony-Thornton <Antony_thornton_p@outlook.com>
2022-06-08 20:40:53 +0000

    Added print to see if the if statement ran.

--- fitness_subscription_app/settings.py
+++ fitness_subscription_app/settings.py
@@ -183,6 +183,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    print('RAN AS EXPECTED')
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'the-veggie-guy-fitness'
     AWS_S3_REGION_NAME = 'eu-west-2'
