0df7462919d2bec8a1b82878fbc51bd6fcf411e3
Antony-Thornton <Antony_thornton_p@outlook.com>
2022-06-13 19:10:23 +0000

    Fixed issue with authorisation.
    Server 500 error on deployed app so fix required.

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -20,14 +21,7 @@ def profile(request):
             messages.error(request, 'Profile update failed.')
     else:
         form = UserProfileForm(instance=profile)
-        orders = profile.orders.all()
-
-        template = 'profiles/profile.html'
-        context = {
-            'form': form,
-            'orders': orders,
-            'on_profile_page': True
-        }
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
